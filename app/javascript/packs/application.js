6550708e92fbfe4f66bb25e0620508fe4f343439
johnnybigoo <jotalico@icloud.com>
2021-02-20 10:02:25 -0500

    Aula-11

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,11 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
 import 'bootstrap'
 import './src/application.scss'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
 
 $(function () {
   'use strict'
@@ -26,10 +18,19 @@ $(function () {
 require("trix")
 require("@rails/actiontext")
 
-$(document).ready(function(){
-  $('#current').text($('.count-textarea').val().length);
-  $('.count-textarea').keyup(function(){
-    var characterCount = $(this).val().length;
-    $('#current').text(characterCount);
-  });
+$(document).ready(function () {
+  if ($('.count-textarea').length > 0) {
+    $('#current').text($('.count-textarea').val().length);
+    $('.count-textarea').keyup(function () {
+      var characterCount = $(this).val().length;
+      $('#current').text(characterCount);
+    });
+  }
+
+  $('.copy-url').click(function (e) {
+    e.preventDefault();
+    var url = $(this).attr('data-clipboard-text');
+    navigator.clipboard.writeText(url);
+    alert('URL copiada com sucesso.')
+  })
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_225139) do
+ActiveRecord::Schema.define(version: 2021_02_20_141809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_225139) do
     t.bigint "company_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["company_id"], name: "index_positions_on_company_id"
   end
 
