a5ebeb873244bd8fed52a960ff68d564ca160dd9
johnnybigoo <jotalico@icloud.com>
2021-06-14 18:38:31 -0400

    open-vagas-Generate-Mailer-2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.welcome.subject
   #
-  def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    def welcome(user)
+      @user = user
+      mail to: @user.email, subject: 'Bem vindo ao Open Vagas'
   end
 end
